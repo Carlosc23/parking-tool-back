e27d7ec9d4ff9d35187c88fb3d81431d1276d0eb
Carlosc23 <cal15219@uvg.edu.gt>
2019-04-16 23:05:30 -0600

    feat: separate methods for retrieve in historical and actual cars

--- connect.py
+++ connect.py
@@ -7,22 +7,35 @@ parking_lot_actual = db[COLLECTION_1]
 parking_lot_historical = db[COLLECTION_2]
 
 
-def add_car(car):
+def add_car_actual(car):
     parking_lot_actual.insert_one(car)
+
+
+def add_car_hist(car):
     parking_lot_historical.insert_one(car)
 
 
-def show_cars():
+def retrieve_hist_cars():
+    result = parking_lot_historical.find()
+    cars_list = []
+    for doc in result:
+        cars_list.append(doc)
+    return cars_list
+
+
+def retrieve_actual_cars():
     result = parking_lot_actual.find()
     cars_list = []
     for doc in result:
         cars_list.append(doc)
+    return cars_list
 
 
 def retrieve_id():
-    id_1 = parking_lot_actual.find()
-    id_2 = parking_lot_historical.find()
-    return id_1, id_2
+    result = parking_lot_historical.find()
+    id_1 = result.count()
+    print(id_1)
+    return int(id_1)
 
 
 """add_car({
